afba42abfadee41e9bc2fcf2d091c40ed5a12b19
miplayer1 <mhiezely@hotmail.fr>
2015-06-11 20:59:40 +0200

    mise en page initiale et redimenssion des images

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Yarn Over</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+		<header>
+			 <nav class="bandeau">
+				<ul>
+					
+					<li class="menu">Présentation</li>
+					<li class="menu">Contact</li>
+					<li class="menu">Réalisations</li>
+				</ul>
+			</nav>
+			
+		</header>
+        <figure id="logo">
+			<img src="images/yarnover.svg" width="250px" height="250" alt="logo style space invader"/></li>
+		</figure>	
+			
+		<section class="gallery">    
+			<ul>
+				<li class="image-container"><img src="images/gallet_crochet.jpg" alt="gallet recouvert de crochet"/></li>   
+				<li class="image-container"><img src="images/yarn_bombing.jpg" alt="personnage crochet accroché à un pillier"/></li>
+				<li class="image-container"><img src="images/projets_collaboratifs.jpg" alt="Poisson au crochet à partir de carrés"/></li>
+				<li class="image-container"><img src="images/autres_projets.jpg" alt="objets de decoration"/></li>  
+			</ul>
+		</section>  
+		
+        <footer>
+         
+        </footer>
+    </body>
+</html>
