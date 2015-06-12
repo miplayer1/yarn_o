e64693579079f14d5c5a90f71ec7454e16860552
mi <mhiezely@hotmail.fr>
2015-06-12 14:40:40 +0200

    nouvelle mise en page et menu adaptatif

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <html>
     <head>
         <meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width"/>
+		<!--[if lt IE 9]>
+		<script src="http://github.com/aFarkas/html5shiv/blob/master/dist/html5shiv.js"></script>
+		<![endif]-->
         <title>Yarn Over</title>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
@@ -9,22 +13,27 @@
 			 <nav class="bandeau">
 				<ul>
 					
-					<li class="menu">Présentation</li>
-					<li class="menu">Contact</li>
-					<li class="menu">Réalisations</li>
+					<li class="menu"id="menu_text">Présentation</li>
+					<li class="menu"id="menu_text">Contact</li>
+					<li class="menu"id="menu_text">Réalisations</li>
+					<li class="menu"><img src="images/TwitterLogo_white.png" alt="oiseau twitter"/></li>
+					<li class="menu"><img src="images/FB_Logo__white.png" alt="facebook"/></li>
+					<li class="menu"><img src="images/pinterest_badge_red.png" alt="pinterest"/></li>
+					<li class ="menu" id="menu_barre"><img src="images/menu_barre.png" alt="menu"/><li>
 				</ul>
 			</nav>
 			
-		</header>
-        <figure id="logo">
-			<img src="images/yarnover.svg" width="250px" height="250" alt="logo style space invader"/></li>
-		</figure>	
 			
+		</header>
+        		
 		<section class="gallery">    
 			<ul>
-				<li class="image-container"><img src="images/gallet_crochet.jpg" alt="gallet recouvert de crochet"/></li>   
+				<li class="image-container"><img src="images/gallet_crochet.jpg" alt="gallet recouvert de crochet"/></li> 
+				<li class="image-container"><img src="images/yarnoverbleu.jpg" alt="logo style space invader"/></li>
 				<li class="image-container"><img src="images/yarn_bombing.jpg" alt="personnage crochet accroché à un pillier"/></li>
+				<li class="image-container"><img src="images/yarnoverpurple.jpg" alt="logo style space invader"/></li>
 				<li class="image-container"><img src="images/projets_collaboratifs.jpg" alt="Poisson au crochet à partir de carrés"/></li>
+				<li class="image-container"><img src="images/yarnoverblack.jpg" alt="logo style space invader"/></li>
 				<li class="image-container"><img src="images/autres_projets.jpg" alt="objets de decoration"/></li>  
 			</ul>
 		</section>  
