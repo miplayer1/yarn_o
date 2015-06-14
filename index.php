667afc6e5aabf925b65a29bb97ad027f7a2cf230
mi <mhiezely@hotmail.fr>
2015-06-14 16:50:53 +0200

    design definitif? avec logo, fond d'écran

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
@@ -8,38 +9,29 @@
         <title>Yarn Over</title>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-    <body>
-		<header>
-			 <nav class="bandeau">
-				<ul>
+	<body>
+				<nav class="bandeau">
 					
-					<li class="menu"id="menu_text">Présentation</li>
-					<li class="menu"id="menu_text">Contact</li>
-					<li class="menu"id="menu_text">Réalisations</li>
-					<li class="menu"><img src="images/TwitterLogo_white.png" alt="oiseau twitter"/></li>
-					<li class="menu"><img src="images/FB_Logo__white.png" alt="facebook"/></li>
-					<li class="menu"><img src="images/pinterest_badge_red.png" alt="pinterest"/></li>
-					<li class ="menu" id="menu_barre"><img src="images/menu_barre.png" alt="menu"/><li>
-				</ul>
-			</nav>
-			
-			
-		</header>
-        		
-		<section class="gallery">    
-			<ul>
-				<li class="image-container"><img src="images/gallet_crochet.jpg" alt="gallet recouvert de crochet"/></li> 
-				<li class="image-container"><img src="images/yarnoverbleu.jpg" alt="logo style space invader"/></li>
-				<li class="image-container"><img src="images/yarn_bombing.jpg" alt="personnage crochet accroché à un pillier"/></li>
-				<li class="image-container"><img src="images/yarnoverpurple.jpg" alt="logo style space invader"/></li>
-				<li class="image-container"><img src="images/projets_collaboratifs.jpg" alt="Poisson au crochet à partir de carrés"/></li>
-				<li class="image-container"><img src="images/yarnoverblack.jpg" alt="logo style space invader"/></li>
-				<li class="image-container"><img src="images/autres_projets.jpg" alt="objets de decoration"/></li>  
-			</ul>
-		</section>  
+					<ul class="menu">
+						<li><img src="images/menu_barre.png" alt="menu" id="menu_deroule"></li>
+						<li>Présentation</li>
+						<li>Contact</li>
+						<li>Réalisations</li>
+												
+						<li><img src="images/TwitterLogo_white.png" alt="oiseau twitter"/></li>
+						<li><img src="images/FB_Logo__white.png" alt="facebook"/></li>
+						<li><img src="images/pinterest_badge_red.png" alt="pinterest"/></li>
+					</ul>
+				</nav>
+				<div class="devise">
+				<p>Maï, créatrice de douceur, dans un monde de brutes...</p>
+				</div>
+				<div class="page">
+				<p><img class="logo"src="images/logomai2.png" alt="logo"/></p>
+				</div>
 		
-        <footer>
-         
-        </footer>
+			
+	
+       
     </body>
 </html>
