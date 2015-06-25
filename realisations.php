7cd7d0b2a2d395424326f48a23af0beed91394f2
miplayer1 <mhiezely@hotmail.fr>
2015-06-25 16:04:27 +0200

    mise en place des box images presentation

---
+++ realisations.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width"/>
+		<!--[if lt IE 9]>
+		<script src="http://github.com/aFarkas/html5shiv/blob/master/dist/html5shiv.js"></script>
+		<![endif]-->
+        <title>Yarn Over</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+	<body>
+				<?php
+				include 'menu.php'; 
+				?>
+			
+				<div class="wrap">
+					<section class ="box">
+						<figure class="imagebox">
+							<img src="images/gallet_crochet.jpg" alt="gallet recouvert au crochet"/>
+						</figure>
+						<figcaption>Illuc ne equos velut per imitatae Sannione ut voragines ut quos equos familiarium spatia dicitur ait imitatae agmina quidem illuc subversasque opertis voluptatum praedatorios praetermitto Sannione agitant relicto relicto trahentes urbis tamquam ait longius imitatae enim per terga ut quos.
+						</figcaption>
+					</section>
+					<section class ="box">
+						<figure class="imagebox">
+							<img src="images/yarn_bombing.jpg" alt="decoration espace urbain avec de la laine"/>
+						</figure>
+						<figcaption>Illuc ne equos velut per imitatae Sannione ut voragines ut quos equos familiarium spatia dicitur ait imitatae agmina quidem illuc subversasque opertis voluptatum praedatorios praetermitto Sannione agitant relicto relicto trahentes urbis tamquam ait longius imitatae enim per terga ut quos.
+						</figcaption>
+					</section>
+					<section class ="box">
+						<figure class="imagebox">
+							<img src="images/projets_collaboratifs.jpg" alt="realisations a plusieurs"/>
+						</figure>
+						<figcaption>Illuc ne equos velut per imitatae Sannione ut voragines ut quos equos familiarium spatia dicitur ait imitatae agmina quidem illuc subversasque opertis voluptatum praedatorios praetermitto Sannione agitant relicto relicto trahentes urbis tamquam ait longius imitatae enim per terga ut quos.
+						<a href="#">En savoir plus</a>
+						</figcaption>
+					</section>
+					<section class ="box">
+						<figure class="imagebox">
+							<img src="images/autres_projets.jpg" alt="autres projets"/>
+						</figure>
+						<figcaption>Illuc ne equos velut per imitatae Sannione ut voragines ut quos equos familiarium spatia dicitur ait imitatae agmina quidem illuc subversasque opertis voluptatum praedatorios praetermitto Sannione agitant relicto relicto trahentes urbis tamquam ait longius imitatae enim per terga ut quos.
+						</figcaption>
+					</section>
+				</div>
+			
+	
+       
+    </body>
+</html>
